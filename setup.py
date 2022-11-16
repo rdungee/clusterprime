5f941994de108c3b6a345bce41fe969d0c7c5a0c
Ryan Dungee <rdungee@hawaii.edu>
2022-11-15 16:45:24 -1000

    setup file for install

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='clusterprime',
+      version='1.0.0',
+      description='turning MEGAPRIME data into Light Curves',
+      url='https://github.com/rdungee/clusterprime',
+      author='Ryan Dungee',
+      author_email='rdungee@hawaii.edu',
+      license='MIT',
+      packages=['clusterprime'],
+      zip_safe=False,
+      install_requires=['numpy == 1.19.5',
+                        'astropy == 4.1',
+                        'photutils == 1.0.2'])
